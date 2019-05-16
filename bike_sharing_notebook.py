945f3fdcf1218b6bf3b1d17195b2886883dd94dd
Jimmy1908 <43721058+Jimmy1908@users.noreply.github.com>
2019-05-16 17:09:18 +0200

    Update

--- bike_sharing_notebook.py
+++ bike_sharing_notebook.py
@@ -19,3 +19,18 @@ from dask_ml.linear_model import LinearRegression
 from dask_ml.metrics import mean_absolute_error
 from dask_ml.metrics import mean_squared_error
 from dask_ml.metrics import r2_score
+
+#Data loading
+day_data = dd.read_csv(
+    "https://gist.githubusercontent.com/geraldwal/483038a2a588e29dbad6ec4e51bbb6bc/raw/152607f899f5746a1b6afa103f68052ffe3fef65/day%2520-%2520Python%2520Bike%2520Sharing",
+    sep=","
+)
+hour_data = dd.read_csv(
+    "https://gist.githubusercontent.com/geraldwal/b5a83f4c670abe0a662abce558e5d433/raw/bce4bbfc63355606e4503964e25798b5d2190b9b/hour%2520-%2520Python%2520Bike%2520Sharing",
+    sep=",",
+    parse_dates=[1]
+)
+location_data = dd.read_csv(
+    "https://gist.githubusercontent.com/geraldwal/3aba0d1a34965e2e59e1b6bf37e0bd91/raw/0266a0f936f2e1e75495d2e351090e80b49c107a/LocationsCapitalBikeStations%2520-%2520Python%2520Group",
+    sep=","
+)
